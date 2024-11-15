38f256ee443055b05af87d9c1c2829c4e543bf3e
Leyla Ahmadova <leylaahmadova24@gmail.com>
2024-11-15 15:39:57 +0400

    delete function was added

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-let container=document.querySelector(".container")
-async function getİtems(){
-    let result=await fetch('https://acb-api.algoritmika.org/api/transaction').then((res)=>res.json())
-    console.log(result);
-    for (let i = 0; i < result.length; i++) {
-        let newProduct=document.createElement("div")
-        newProduct.classList.add("item")
-        newProduct.innerHTML=`
+let container = document.querySelector(".container");
+async function getİtems() {
+  let result = await fetch(
+    "https://acb-api.algoritmika.org/api/transaction"
+  ).then((res) => res.json());
+  console.log(result);
+  for (let i = 0; i < result.length; i++) {
+    let newProduct = document.createElement("div");
+    newProduct.classList.add("item");
+    newProduct.innerHTML = `
         <div class="info">
-            <p>from: ${result[i].from}}</p>
-            <p>to: ${result[i].to}}</p>
-            <p>amount: ${result[i].amount}}</p>
-            <p>id: ${result[i].id}</p>
+            <p>from: ${result[i].from}</p>
+            <p>to: ${result[i].to}</p>
+            <p>amount: ${result[i].amount}</p>
+            <p class="id">${result[i].id}</p>
             
         </div>
         <div class="editButton">edit</div>
-        <div class="deleteButton">delete</div>
-        `
-        container.appendChild(newProduct)
-        
-    }
+        <div class="deleteButton" onclick="sil(this)">delete</div>
+        `;
+    container.appendChild(newProduct);
+  }
 }
-getİtems()
+getİtems();
+function sil(element) {
+  let idElement = element.parentElement;
+  let id = idElement.querySelector(".id").innerHTML;
+  fetch(`https://acb-api.algoritmika.org/api/transaction/${id}`, {
+    method: "DELETE",
+  });
+}
