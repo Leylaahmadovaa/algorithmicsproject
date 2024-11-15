3cd328c6f969d3c6651128f1cf2b52be43c01930
Leyla Ahmadova <leylaahmadova24@gmail.com>
2024-11-15 16:25:25 +0400

    edit/save functions were added

--- script.js
+++ script.js
@@ -9,23 +9,64 @@ async function getİtems() {
     newProduct.classList.add("item");
     newProduct.innerHTML = `
         <div class="info">
-            <p>from: ${result[i].from}</p>
-            <p>to: ${result[i].to}</p>
-            <p>amount: ${result[i].amount}</p>
+            <span>from:</span><input class="from" type="text" value="${result[i].from}" readonly> 
+            <span>to:</span><input class="to" type="text" value="${result[i].to}" readonly>
+            <span>amount:</span><input class="amount" type="text" value="${result[i].amount}" readonly>
             <p class="id">${result[i].id}</p>
-            
-        </div>
-        <div class="editButton">edit</div>
-        <div class="deleteButton" onclick="sil(this)">delete</div>
-        `;
-    container.appendChild(newProduct);
-  }
-}
+            </div>
+            <div class="editButton" onclick="edit(this)">edit</div>
+            <div class="saveButton" onclick="save(this)">save</div>
+            <div class="deleteButton" onclick="sil(this)">delete</div>
+            `;
+            container.appendChild(newProduct);
+        }
+    }
 getİtems();
 function sil(element) {
-  let idElement = element.parentElement;
+  let idElement = element.parentElement;//item
   let id = idElement.querySelector(".id").innerHTML;
   fetch(`https://acb-api.algoritmika.org/api/transaction/${id}`, {
     method: "DELETE",
   });
+  idElement.remove();
 }
+
+function edit(element){
+    let e = element.parentElement;//item
+    let from=e.querySelector(".from");
+    let to=e.querySelector(".to");
+    let amount=e.querySelector(".amount");
+    from.removeAttribute("readonly");       
+    to.removeAttribute("readonly");       
+    amount.removeAttribute("readonly");       
+}
+function save(element){
+    let e = element.parentElement;//item
+    let from=e.querySelector(".from");
+    let to=e.querySelector(".to");
+    let amount=e.querySelector(".amount");
+    
+    from.setAttribute('value', `${from.value}`);
+    to.setAttribute('value', `${to.value}`);
+    amount.setAttribute('value', `${amount.value}`);
+
+    from.setAttribute('readonly', true);
+    to.setAttribute('readonly', true);
+    amount.setAttribute('readonly', true);
+  
+  let ID = e.querySelector(".id").innerHTML;
+    const info = {
+        from: from.value,
+        to: to.value,
+        amount: amount.value,
+        id:ID
+      }
+      fetch(`https://acb-api.algoritmika.org/api/transaction/${ID}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(info)
+      });
+
+  }
